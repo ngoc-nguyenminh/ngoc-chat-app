6745da88c74199bc70a680a06fe4de628d440e8a
Nguyen Minh Ngoc <ngoc.nguyenminhhcmut@gmail.com>
2023-09-17 12:01:04 +0700

    add layout; add signout button; handle friend request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Button from '@/components/ui/Button';
-import { db } from '@/lib/db';
 
 export default async function Home() {
-  await db.set('hi', 'hi');
   return (
     <Button size='lg' variant='default'>
       Hallo
